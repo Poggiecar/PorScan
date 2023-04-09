245655503c240e5e3480f7a5853e20f807a6ae3f
Ecar Poggi <poggiecar@gmail.com>
2023-04-08 23:34:31 -0300

    Se agrega el archivo

---
+++ scan.py
@@ -0,0 +1,50 @@
+#!/user/bin/python3
+
+import nmap
+
+
+print("  (^^^^^^^^^^^)")
+print("  ( -o-   -o- )")
+print("  (     J     )")
+print("  (   /---/   )")
+print("  (    ⏝⏝    )")
+
+print("     ")
+print("     ")
+
+
+
+
+
+
+print("  [] Herramienta para escanear puertos abiertos en una direccion IP")
+print("  [] Escrita en Python con Sublime, utiliza Nmap")
+print("  [] Proximamente nuevas veriones")
+
+nombre_escaneo= input ("[+] Nombre del escaneo: ")
+ip= input("[+] Ip objetivo: ")
+nm= nmap.PortScanner()
+results= nm.scan(ip)
+puertos_abiertos="-p"
+count=0
+results= nm.scan(hosts=ip, arguments=" -sT -n -Pn -T3 ")
+print("\nHost : %s" % ip)
+print("State : %s" % nm[ip].state())
+for proto in nm[ip].all_protocols():
+    print("Protocol : %s" % proto)
+    lport = nm[ip][proto].keys()
+    sorted(lport)
+    for port in lport:
+        print ('port : %s\tstate : %s' % (port, nm[ip][proto][port]['state']))
+        if count==0:
+            puertos_abiertos= puertos_abiertos + " " + str(port)
+            count=1
+        else:
+            puertos_abiertos= puertos_abiertos + "," + str(port)
+
+print("Puertos abiertos: "+puertos_abiertos+ " " + str(ip)) 	
+print("Escaneo con Scripts y versiones: nmap -sC -sV "+puertos_abiertos + " " + str(ip) + " -oN "+ str(nombre_escaneo)+".txt")		
+
+
+
+#print(results)
